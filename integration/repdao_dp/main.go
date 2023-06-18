491ed3c11f87ee5754c0f636c02d64382a24cc4c
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2023-06-17 17:47:32 -0700

    Fix repdao for dp instances to allow one instance per run

--- integration/repdao_dp/main.go
+++ integration/repdao_dp/main.go
@@ -40,188 +40,184 @@ func main() {
 	}
 	defer repdaoMongo.Disconnect(ctx)
 
-	countryCollectionMap := map[string]string{
-		"US": "retrievalbot_1",
-		"DE": "retrievalbot_2",
-		"SG": "retrievalbot_3",
+	country := os.Getenv("RETRIEVER_COUNTRY")
+	collectionName := os.Getenv("REPDAO_MONGO_COLLECTION")
+
+	repdao := repdaoMongo.Database(os.Getenv("REPDAO_MONGO_DATABASE")).Collection(collectionName)
+
+	// Find the last saved date
+	var lastStats DailyStats
+	err = repdao.FindOne(ctx, bson.D{}, options.FindOne().SetSort(bson.D{{"date", -1}})).Decode(&lastStats)
+	if err != nil && err != mongo.ErrNoDocuments {
+		panic(err)
 	}
 
-	for country, collectionName := range countryCollectionMap {
-		repdao := repdaoMongo.Database(os.Getenv("REPDAO_MONGO_DATABASE")).Collection(collectionName)
-
-		// Find the last saved date
-		var lastStats DailyStats
-		err = repdao.FindOne(ctx, bson.D{}, options.FindOne().SetSort(bson.D{{"date", -1}})).Decode(&lastStats)
-		if err != nil && err != mongo.ErrNoDocuments {
+	var startDate time.Time
+	if err == mongo.ErrNoDocuments {
+		startDate = time.Time{}
+	} else {
+		startDate, err = time.Parse("2006-01-02", lastStats.Date)
+		if err != nil {
 			panic(err)
 		}
+		startDate = startDate.AddDate(0, 0, 1)
+	}
 
-		var startDate time.Time
-		if err == mongo.ErrNoDocuments {
-			startDate = time.Time{}
-		} else {
-			startDate, err = time.Parse("2006-01-02", lastStats.Date)
-			if err != nil {
-				panic(err)
-			}
-			startDate = startDate.AddDate(0, 0, 1)
-		}
+	// Get the current day part of yesterday
+	now := time.Now().UTC()
+	endDate := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 
-		// Get the current day part of yesterday
-		now := time.Now().UTC()
-		endDate := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	fmt.Printf("startDate: %s, endDate: %s\n", startDate, endDate)
+	if startDate.After(endDate) || startDate.Equal(endDate) {
+		fmt.Println("No new data to process")
+		return
+	}
 
-		fmt.Printf("startDate: %s, endDate: %s\n", startDate, endDate)
-		if startDate.After(endDate) || startDate.Equal(endDate) {
-			fmt.Println("No new data to process")
-			return
-		}
+	// Aggregate the results
+	matchStage := bson.D{{"$match", bson.D{
+		{"retriever.country", country},
+		{"created_at", bson.D{{"$gte", startDate}, {"$lt", endDate}}},
+		{"task.module", bson.D{{"$in", bson.A{"http", "bitswap", "graphsync"}}}},
+	}}}
 
-		// Aggregate the results
-		matchStage := bson.D{{"$match", bson.D{
-			{"retriever.country", country},
-			{"created_at", bson.D{{"$gte", startDate}, {"$lt", endDate}}},
-			{"task.module", bson.D{{"$in", bson.A{"http", "bitswap", "graphsync"}}}},
-		}}}
-
-		groupStage := bson.D{{"$group", bson.D{
-			{"_id", bson.D{
-				{"provider_id", "$task.provider.id"},
-				{"date", bson.D{{"$dateToString", bson.D{
-					{"format", "%Y-%m-%d"},
-					{"date", "$created_at"},
-				}}}},
-				{"module", "$task.module"},
-				{"success", "$result.success"},
-			}},
-			{"count", bson.D{{"$sum", 1}}},
-			{"ttfb_sum", bson.D{{"$sum", "$result.ttfb"}}},
-			{"speed_sum", bson.D{{"$sum", "$result.speed"}}},
-		}}}
-
-		groupStage2 := bson.D{{"$group", bson.D{
-			{"_id", bson.D{
-				{"provider_id", "$_id.provider_id"},
-				{"date", "$_id.date"},
-			}},
-			{"http_retrievals", bson.D{{"$sum", bson.D{
-				{"$cond", bson.A{
-					bson.D{{"$eq", bson.A{"$_id.module", "http"}}},
-					"$count",
-					0,
-				}},
+	groupStage := bson.D{{"$group", bson.D{
+		{"_id", bson.D{
+			{"provider_id", "$task.provider.id"},
+			{"date", bson.D{{"$dateToString", bson.D{
+				{"format", "%Y-%m-%d"},
+				{"date", "$created_at"},
 			}}}},
-			{"http_retrieval_success", bson.D{{"$sum", bson.D{
-				{"$cond", bson.A{
-					bson.D{{"$and", bson.A{
-						bson.D{{"$eq", bson.A{"$_id.module", "http"}}},
-						bson.D{{"$eq", bson.A{"$_id.success", true}}},
-					}}},
-					"$count",
-					0,
-				}}},
-			}}},
-			{"bitswap_retrievals", bson.D{{"$sum", bson.D{
-				{"$cond", bson.A{
-					bson.D{{"$eq", bson.A{"$_id.module", "bitswap"}}},
-					"$count",
-					0,
-				}},
-			}}}},
-			{"bitswap_retrieval_success", bson.D{{"$sum", bson.D{
-				{"$cond", bson.A{
-					bson.D{{"$and", bson.A{
-						bson.D{{"$eq", bson.A{"$_id.module", "bitswap"}}},
-						bson.D{{"$eq", bson.A{"$_id.success", true}}},
-					}}},
-					"$count",
-					0,
-				}}},
-			}}},
-			{"graphsync_retrievals", bson.D{{"$sum", bson.D{
-				{"$cond", bson.A{
-					bson.D{{"$eq", bson.A{"$_id.module", "graphsync"}}},
-					"$count",
-					0,
-				}},
-			}}}},
-			{"graphsync_retrieval_success", bson.D{{"$sum", bson.D{
-				{"$cond", bson.A{
-					bson.D{{"$and", bson.A{
-						bson.D{{"$eq", bson.A{"$_id.module", "graphsync"}}},
-						bson.D{{"$eq", bson.A{"$_id.success", true}}},
-					}}},
-					"$count",
-					0,
-				}}},
-			}}},
-			{"ttfb_sum", bson.D{{"$sum", bson.D{
-				{"$cond", bson.A{
-					bson.D{{"$eq", bson.A{"$_id.success", true}}},
-					"$ttfb_sum",
-					0,
-				}},
-			}}}},
-			{"speed_sum", bson.D{{"$sum", bson.D{
-				{"$cond", bson.A{
-					bson.D{{"$eq", bson.A{"$_id.success", true}}},
-					"$speed_sum",
-					0,
-				}},
-			}}}},
-			{"success_count", bson.D{{"$sum", bson.D{
-				{"$cond", bson.A{
-					bson.D{{"$eq", bson.A{"$_id.success", true}}},
-					"$count",
-					0,
-				}},
-			}}}},
-		}}}
+			{"module", "$task.module"},
+			{"success", "$result.success"},
+		}},
+		{"count", bson.D{{"$sum", 1}}},
+		{"ttfb_sum", bson.D{{"$sum", "$result.ttfb"}}},
+		{"speed_sum", bson.D{{"$sum", "$result.speed"}}},
+	}}}
 
-		projectStage := bson.D{{"$project", bson.D{
+	groupStage2 := bson.D{{"$group", bson.D{
+		{"_id", bson.D{
 			{"provider_id", "$_id.provider_id"},
 			{"date", "$_id.date"},
-			{"http_retrievals", 1},
-			{"http_retrieval_success", 1},
-			{"bitswap_retrievals", 1},
-			{"bitswap_retrieval_success", 1},
-			{"graphsync_retrievals", 1},
-			{"graphsync_retrieval_success", 1},
-			{"avg_ttfb_ms", bson.D{{"$cond", bson.A{
-				bson.D{{"$eq", bson.A{"$success_count", 0}}},
+		}},
+		{"http_retrievals", bson.D{{"$sum", bson.D{
+			{"$cond", bson.A{
+				bson.D{{"$eq", bson.A{"$_id.module", "http"}}},
+				"$count",
 				0,
-				bson.D{{"$divide", bson.A{bson.D{{"$divide", bson.A{"$ttfb_sum", "$success_count"}}}, 1000000.0}}},
-			}}}},
-			{"avg_speed_bps", bson.D{{"$cond", bson.A{
-				bson.D{{"$eq", bson.A{"$success_count", 0}}},
+			}},
+		}}}},
+		{"http_retrieval_success", bson.D{{"$sum", bson.D{
+			{"$cond", bson.A{
+				bson.D{{"$and", bson.A{
+					bson.D{{"$eq", bson.A{"$_id.module", "http"}}},
+					bson.D{{"$eq", bson.A{"$_id.success", true}}},
+				}}},
+				"$count",
 				0,
-				bson.D{{"$divide", bson.A{"$speed_sum", "$success_count"}}},
-			}}}},
-		}}}
+			}}},
+		}}},
+		{"bitswap_retrievals", bson.D{{"$sum", bson.D{
+			{"$cond", bson.A{
+				bson.D{{"$eq", bson.A{"$_id.module", "bitswap"}}},
+				"$count",
+				0,
+			}},
+		}}}},
+		{"bitswap_retrieval_success", bson.D{{"$sum", bson.D{
+			{"$cond", bson.A{
+				bson.D{{"$and", bson.A{
+					bson.D{{"$eq", bson.A{"$_id.module", "bitswap"}}},
+					bson.D{{"$eq", bson.A{"$_id.success", true}}},
+				}}},
+				"$count",
+				0,
+			}}},
+		}}},
+		{"graphsync_retrievals", bson.D{{"$sum", bson.D{
+			{"$cond", bson.A{
+				bson.D{{"$eq", bson.A{"$_id.module", "graphsync"}}},
+				"$count",
+				0,
+			}},
+		}}}},
+		{"graphsync_retrieval_success", bson.D{{"$sum", bson.D{
+			{"$cond", bson.A{
+				bson.D{{"$and", bson.A{
+					bson.D{{"$eq", bson.A{"$_id.module", "graphsync"}}},
+					bson.D{{"$eq", bson.A{"$_id.success", true}}},
+				}}},
+				"$count",
+				0,
+			}}},
+		}}},
+		{"ttfb_sum", bson.D{{"$sum", bson.D{
+			{"$cond", bson.A{
+				bson.D{{"$eq", bson.A{"$_id.success", true}}},
+				"$ttfb_sum",
+				0,
+			}},
+		}}}},
+		{"speed_sum", bson.D{{"$sum", bson.D{
+			{"$cond", bson.A{
+				bson.D{{"$eq", bson.A{"$_id.success", true}}},
+				"$speed_sum",
+				0,
+			}},
+		}}}},
+		{"success_count", bson.D{{"$sum", bson.D{
+			{"$cond", bson.A{
+				bson.D{{"$eq", bson.A{"$_id.success", true}}},
+				"$count",
+				0,
+			}},
+		}}}},
+	}}}
 
-		cursor, err := retbot.Aggregate(context.Background(), mongo.Pipeline{matchStage, groupStage, groupStage2, projectStage})
-		if err != nil {
-			panic(err)
-		}
-		defer cursor.Close(context.Background())
-		var stats []interface{}
+	projectStage := bson.D{{"$project", bson.D{
+		{"provider_id", "$_id.provider_id"},
+		{"date", "$_id.date"},
+		{"http_retrievals", 1},
+		{"http_retrieval_success", 1},
+		{"bitswap_retrievals", 1},
+		{"bitswap_retrieval_success", 1},
+		{"graphsync_retrievals", 1},
+		{"graphsync_retrieval_success", 1},
+		{"avg_ttfb_ms", bson.D{{"$cond", bson.A{
+			bson.D{{"$eq", bson.A{"$success_count", 0}}},
+			0,
+			bson.D{{"$divide", bson.A{bson.D{{"$divide", bson.A{"$ttfb_sum", "$success_count"}}}, 1000000.0}}},
+		}}}},
+		{"avg_speed_bps", bson.D{{"$cond", bson.A{
+			bson.D{{"$eq", bson.A{"$success_count", 0}}},
+			0,
+			bson.D{{"$divide", bson.A{"$speed_sum", "$success_count"}}},
+		}}}},
+	}}}
 
-		// Insert the aggregated results into the new collection
-		for cursor.Next(context.Background()) {
-			var dailyStats DailyStats
-			err := cursor.Decode(&dailyStats)
-			if err != nil {
-				panic(err)
-			}
-			stats = append(stats, dailyStats)
-			fmt.Printf("Got daily stats: %+v\n", dailyStats)
-		}
-
-		// Insert the aggregated results into the new collection
-		result, err := repdao.InsertMany(ctx, stats)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Inserted %v documents into the new collection!\n", len(result.InsertedIDs))
+	cursor, err := retbot.Aggregate(context.Background(), mongo.Pipeline{matchStage, groupStage, groupStage2, projectStage})
+	if err != nil {
+		panic(err)
 	}
+	defer cursor.Close(context.Background())
+	var stats []interface{}
+
+	// Insert the aggregated results into the new collection
+	for cursor.Next(context.Background()) {
+		var dailyStats DailyStats
+		err := cursor.Decode(&dailyStats)
+		if err != nil {
+			panic(err)
+		}
+		stats = append(stats, dailyStats)
+		fmt.Printf("Got daily stats: %+v\n", dailyStats)
+	}
+
+	// Insert the aggregated results into the new collection
+	result, err := repdao.InsertMany(ctx, stats)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Inserted %v documents into the new collection!\n", len(result.InsertedIDs))
+
 }
