79692a5273758ecc3bada8a6846ef0705d505d9f
stephen-pl <139834872+stephen-pl@users.noreply.github.com>
2023-08-17 21:46:34 -0700

    Caching provider info
    API gatway doesnt play well with rpc so had to make the call using
    http.

--- pkg/resolver/provider_resolver.go
+++ pkg/resolver/provider_resolver.go
@@ -5,19 +5,21 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"net/http"
-	"net/rpc/jsonrpc"
+	"os"
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/jellydator/ttlcache/v3"
 	"github.com/pkg/errors"
+	"github.com/ybbus/jsonrpc/v3"
 )
 
 type ProviderResolver struct {
-	cache *ttlcache.Cache[string, MinerInfo]
-	url   string
-	token string
+	cache       *ttlcache.Cache[string, MinerInfo]
+	lotusClient jsonrpc.RPCClient
+	url         string
+	token       string
 }
 
 type MinerInfo struct {
@@ -33,20 +35,25 @@ func NewProviderResolver(url string, token string, ttl time.Duration) (*Provider
 		//nolint:gomnd
 		ttlcache.WithTTL[string, MinerInfo](ttl),
 		ttlcache.WithDisableTouchOnHit[string, MinerInfo]())
-	// var lotusClient jsonrpc.RPCClient
-	// if token == "" {
-	// 	lotusClient = jsonrpc.NewClient(url)
-	// } else {
-	// 	lotusClient = jsonrpc.NewClientWithOpts(url, &jsonrpc.RPCClientOpts{
-	// 		CustomHeaders: map[string]string{
-	// 			"Authorization": "Bearer " + token,
-	// 		},
-	// 	})
-	// }
+
+	var lotusClient jsonrpc.RPCClient
+	if os.Getenv("USE_REMOTE_LOTUS_CACHE_HTTP") == "" {
+		if token == "" {
+			lotusClient = jsonrpc.NewClient(url)
+		} else {
+			lotusClient = jsonrpc.NewClientWithOpts(url, &jsonrpc.RPCClientOpts{
+				CustomHeaders: map[string]string{
+					"Authorization": "Bearer " + token,
+				},
+			})
+		}
+	}
+
 	return &ProviderResolver{
-		cache: cache,
-		url:   url,
-		token: token,
+		cache:       cache,
+		lotusClient: lotusClient,
+		url:         url,
+		token:       token,
 	}, nil
 }
 
@@ -55,54 +62,71 @@ func (p *ProviderResolver) ResolveProvider(ctx context.Context, provider string)
 	if minerInfo := p.cache.Get(provider); minerInfo != nil && !minerInfo.IsExpired() {
 		return minerInfo.Value(), nil
 	}
-	var lotusClient jsonrpc.RPCClient
-	// if token == "" {
-	lotusClient = jsonrpc.NewClient("test")
-	//logger.With("provider", provider).Debug("Getting miner info")
-
-	url := p.url + "?provider=" + provider
-	if p.token != "" {
-		url = url + "&token=" + p.token
-	}
-
-	request, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-	if err != nil {
-		return MinerInfo{}, errors.Wrap(err, "failed to create http request")
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(request)
-
-	if err != nil {
-		return MinerInfo{}, errors.Wrap(err, "failed to get miner info")
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return MinerInfo{}, errors.New("failed to get miner info: " + resp.Status)
-	}
-
-	var minerInfo MinerInfo
-	err = json.NewDecoder(resp.Body).Decode(&minerInfo)
-	if err != nil {
-		return MinerInfo{}, errors.Wrap(err, "failed to decode miner info")
-	}
-
-	//err := p.lotusClient.CallFor(ctx, minerInfo, "Filecoin.StateMinerInfo", provider, nil)
-	if err != nil {
-		return MinerInfo{}, errors.Wrap(err, "failed to get miner info")
-	}
-
-	logger.With("provider", provider, "minerinfo", minerInfo).Debug("Got miner info")
-	minerInfo.Multiaddrs = make([]abi.Multiaddrs, len(minerInfo.MultiaddrsBase64Encoded))
-	for i, multiaddr := range minerInfo.MultiaddrsBase64Encoded {
-		decoded, err := base64.StdEncoding.DecodeString(multiaddr)
+	//nolint:nestif
+	if os.Getenv("USE_REMOTE_LOTUS_CACHE_HTTP") == "" {
+		logger.With("provider", provider).Debug("Getting miner info")
+		minerInfo := new(MinerInfo)
+		err := p.lotusClient.CallFor(ctx, minerInfo, "Filecoin.StateMinerInfo", provider, nil)
 		if err != nil {
-			return MinerInfo{}, errors.Wrap(err, "failed to decode multiaddr")
+			return MinerInfo{}, errors.Wrap(err, "failed to get miner info")
 		}
-		minerInfo.Multiaddrs[i] = decoded
-	}
-	p.cache.Set(provider, minerInfo, ttlcache.DefaultTTL)
+		logger.With("provider", provider, "minerinfo", minerInfo).Debug("Got miner info")
+		minerInfo.Multiaddrs = make([]abi.Multiaddrs, len(minerInfo.MultiaddrsBase64Encoded))
+		for i, multiaddr := range minerInfo.MultiaddrsBase64Encoded {
+			decoded, err := base64.StdEncoding.DecodeString(multiaddr)
+			if err != nil {
+				return MinerInfo{}, errors.Wrap(err, "failed to decode multiaddr")
+			}
+			minerInfo.Multiaddrs[i] = decoded
+		}
 
-	return minerInfo, nil
+		p.cache.Set(provider, *minerInfo, ttlcache.DefaultTTL)
+		return *minerInfo, nil
+	} else {
+		logger := logging.Logger("location_resolver")
+		url := p.url + "?provider=" + provider
+		if p.token != "" {
+			url = url + "&token=" + p.token
+		}
+
+		request, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+		if err != nil {
+			return MinerInfo{}, errors.Wrap(err, "failed to create http request")
+		}
+
+		client := &http.Client{}
+		resp, err := client.Do(request)
+
+		if err != nil {
+			return MinerInfo{}, errors.Wrap(err, "failed to get miner info")
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return MinerInfo{}, errors.New("failed to get miner info: " + resp.Status)
+		}
+
+		var minerInfo MinerInfo
+		err = json.NewDecoder(resp.Body).Decode(&minerInfo)
+		if err != nil {
+			return MinerInfo{}, errors.Wrap(err, "failed to decode miner info")
+		}
+
+		if err != nil {
+			return MinerInfo{}, errors.Wrap(err, "failed to get miner info")
+		}
+
+		logger.With("provider", provider, "minerinfo", minerInfo).Debug("Got miner info")
+		minerInfo.Multiaddrs = make([]abi.Multiaddrs, len(minerInfo.MultiaddrsBase64Encoded))
+		for i, multiaddr := range minerInfo.MultiaddrsBase64Encoded {
+			decoded, err := base64.StdEncoding.DecodeString(multiaddr)
+			if err != nil {
+				return MinerInfo{}, errors.Wrap(err, "failed to decode multiaddr")
+			}
+			minerInfo.Multiaddrs[i] = decoded
+		}
+		p.cache.Set(provider, minerInfo, ttlcache.DefaultTTL)
+
+		return minerInfo, nil
+	}
 }
